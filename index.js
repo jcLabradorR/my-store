20eaf57c954c9cba7397616d74d097763870fafa
Julio Labrador <juliotelcel@hotmail.com>
2022-07-16 04:53:24 -0400

    creacion de whitelist cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -7,6 +8,18 @@ const app = express();
 const port = 3128;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://my2wapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get("/", (req, res) =>{
   res.send("Hola mi server en Express");
